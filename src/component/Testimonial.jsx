26a77ebb5739e23dcea5b47b7ed668a4e2e30b65
Forhad Khan <fk.nafi11@gmail.com>
2022-12-11 22:02:27 +0600

    update footer

--- src/component/Testimonial.jsx
+++ src/component/Testimonial.jsx
@@ -10,7 +10,7 @@ function Testimonial() {
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 justify-center items-baseline space-y-4 lg:space-y-0 mt-16 lg:mt-28 px-0 lg:px-6'>
         
       <div className='flex flex-col justify-center items-center space-y-4 bg-slate-900 rounded-lg p-3'>
-          <img src="https://www.fillmurray.com/640/640" className='rounded-full w-1/2' alt="" />
+          <img src="https://picsum.photos/640" className='rounded-full w-1/2' alt="" />
           <div>
             <h2 className='font-bold text-xl space-y-3'>
                  John Doe
